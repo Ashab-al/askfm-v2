f0bb663b99f04de3afe7c4bfa95570352d236f3b
Ashab-al <asxabal7@gmail.com>
2023-10-04 20:21:09 +0300

    filled in the controllers/question.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,69 @@
 class QuestionsController < ApplicationController
-  def index
-  end
+  before_action :check_author_id, only: [:update]
+  before_action :checking_for_nil_text_author_name, only: [:create, :update]
 
-  def show
+  
+  def index
+    @questions = Question.all
   end
 
   def new
-  end
-
-  def create
+    
   end
 
   def edit
   end
 
-  def update
+  def destroy
+
   end
 
-  def destroy
+  def show
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_update_params)
+      redirect_back fallback_location: root_path, notice: "Вопрос успешно обновлен"
+    else
+      render 'edit'
+    end
+  end
+  
+
+  def create
+    @question = Question.new(question_params)
+    #QuestionManager::QuestionCreator.call(question_params)
+
+    if @question.save
+      redirect_back fallback_location: root_path, notice: "Вопрос успешно добавлен"
+    else
+      render 'new'
+    end
+    
+  end
+  
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text, :author_name, :user_id, :author_id)
+  end
+
+  def question_update_params
+    params.require(:question).permit(:answer, :text)
+  end
+
+  # def check_author_id
+  #   if question_params[:author_id].present? && (current_user.id != question_params[:author_id])
+  #     redirect_to root_path, alert: 'Доступ запрещён'
+  #   end
+  # end
+  
+  def checking_for_nil_text_author_name
+    unless question_params[:text].present? || question_params[:author_name].present?
+      redirect_back fallback_location: root_path, notice: "Поля должны быть заполнены"
+    end
   end
 end
