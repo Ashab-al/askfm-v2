7689bf40bed9e993fc47a4160721fc3256ae4b8b
Ashab-al <asxabal7@gmail.com>
2023-10-11 04:14:33 +0300

    successfully connected 'device confirmable' and gem 'mailcatcher'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,13 +35,14 @@ Rails.application.configure do
 
   # action mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   # SMPT settings for mailcatcher gem
-  config.action_mailer.smpt_settings = {
-    
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port: 1025
   }
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
