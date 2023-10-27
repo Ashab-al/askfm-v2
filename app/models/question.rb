caae3100b70a0af9439d57b665d1acdc6b3c1aa1
Allen <allenonrails@zohomail.eu>
2023-10-27 16:59:00 +0300

    create tags after question save

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,19 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true 
-  has_many :question_tags
+  has_many :question_tags, dependent: :destroy
   has_many :tags, through: :question_tags
 
   validates :text, presence: true, length: { maximum: 255 }
 
+  after_save :create_tags_from_question_text
+
+  def create_tags_from_question_text
+    tags = text.scan(/#\w+/)
+
+    tags.each do |tag_text|
+      tag = Tag.find_or_create_by(title: tag_text[1..-1])
+      question_tags.find_or_create_by(tag: tag)
+    end
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,6 @@
 class Tag < ApplicationRecord
-    has_many :question_tags
-    has_many :questions, through: :question_tags
-    
-    validates :title, uniqueness: true
-
+  has_many :question_tags
+  has_many :questions, through: :question_tags
+  
+  validates :title, uniqueness: true
 end
